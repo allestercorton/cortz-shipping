c049eba9bd80311c42fed913b060c12b4cc56141
Allester <cortonallester@gmail.com>
2025-02-14 11:41:09 +0800

    payment method

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -20,6 +20,7 @@ import CartPage from './pages/CartPage.tsx';
 import ShippingAddressPage from './pages/ShippingAddressPage.tsx';
 import SigninPage from './pages/SigninPage.tsx';
 import SignupPage from './pages/SignupPage.tsx';
+import PaymentMethodPage from './pages/PaymentMethodPage.tsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -30,6 +31,7 @@ const router = createBrowserRouter(
       <Route path='signin' element={<SigninPage />} />
       <Route path='signup' element={<SignupPage />} />
       <Route path='shipping' element={<ShippingAddressPage />} />
+      <Route path='payment' element={<PaymentMethodPage />} />
     </Route>
   )
 );
